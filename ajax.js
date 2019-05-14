fe65cd93d698264c33e6e972000f9ae0ae4f9a4b
weston-ryan <31858652+weston-ryan@users.noreply.github.com>
2019-05-13 20:31:41 -0600

    Update ajax.js
    updated some comments

--- ajax.js
+++ ajax.js
@@ -1,17 +1,25 @@
+//create a variable to hold the html button item
 var button = document.getElementById('fetch');
 
+//add an event listener to button
 button.addEventListener('click',function(){
-
+    
+//create an instance of XMLHTTPRequest
     var getMyData = new XMLHttpRequest();
 
+    //establish the connection
     getMyData.open('GET','http://dev.virtualearth.net/REST/v1/Locations?q=san%20francisco&output=json&key=AiHEmAatcY9KyySrdotAy9AkyhZqcGZegE9pRvecG25MQpbShbWNYGmj8YzmLBKj');
 
+    //if successfull, then continue
     getMyData.onload = function () {
 
+        //parse the JSON string returned by the API web service server
         var info = JSON.parse(getMyData.responseText);
 
+       //print the parsed data into the console for debugging
         console.log(info);
 
+        //call the function responsible to display the data in the html page
         var cityName = info.resourceSets[0].resources[0].name;
         document.getElementById('name').innerHTML = cityName;
 
