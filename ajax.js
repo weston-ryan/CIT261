65bcc2aaf43e20b105513a9d94e18deb4e234b30
weston-ryan <31858652+weston-ryan@users.noreply.github.com>
2019-05-13 20:01:08 -0600

    Update ajax.js

--- ajax.js
+++ ajax.js
@@ -1,27 +1,36 @@
+//create a variable to hold the html button item
 var button = document.getElementById('fetch');
 
+//add an event listener to button
 button.addEventListener('click',function(){
 
-    var getMyData = new XMLHttpRequest();
+//create an instance of XMLHTTPRequest
+var getMyData = new XMLHttpRequest();
 
-    getMyData.open('GET','http://dev.virtualearth.net/REST/v1/Locations?q=san%20francisco&output=json&key=AiHEmAatcY9KyySrdotAy9AkyhZqcGZegE9pRvecG25MQpbShbWNYGmj8YzmLBKj');
+//establish the connection
+getMyData.open('GET','http://dev.virtualearth.net/REST/v1/Locations?q=san%20francisco&output=json&key=AiHEmAatcY9KyySrdotAy9AkyhZqcGZegE9pRvecG25MQpbShbWNYGmj8YzmLBKj');
 
-    getMyData.onload = function () {
 
-        var info = JSON.parse(getMyData.responseText);
+//if successfull, then continue
+getMyData.onload = function () {
 
-        console.log(info);
+//parse the JSON string returned by the API web service server
+var info = JSON.parse(getMyData.responseText);
 
-        var cityName = info.resourceSets[0].resources[0].name;
-        document.getElementById('name').innerHTML = cityName;
+//print the parsed data into the console for debugging
+console.log(info);
 
-        var latitude = info.resourceSets[0].resources[0].point.coordinates[0];
-        document.getElementById('lat').innerHTML = latitude;
+//call the function responsible to display the data in the html page
 
-        var longitude = info.resourceSets[0].resources[0].point.coordinates[1];
-        document.getElementById('long').innerHTML = longitude;
-    }
+var cityName = info.resourceSets[0].resources[0].name;
+var latitude = info.resourceSets[0].resources[0].point.coordinates[0];
+var longitude = info.resourceSets[0].resources[0].point.coordinates[1];
 
-    getMyData.send();
+document.getElementById('name').innerHTML = cityName;
+document.getElementById('lat').innerHTML = latitude;
+document.getElementById('long').innerHTML = longitude;
+}
+
+getMyData.send();
 
 });
