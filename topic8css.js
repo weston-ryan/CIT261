52fcf5dde21124d5e8a42dff22db8c53bdfc5ad8
weston-ryan <31858652+weston-ryan@users.noreply.github.com>
2019-05-25 12:28:04 -0600

    Update topic8css.js

--- topic8css.js
+++ topic8css.js
@@ -1,15 +1,20 @@
-function myFunction() {
-    document.getElementById("myHover").style.WebkitTransitionProperty = "width, height"; // Code for Safari 3.1 to 6.0
-    document.getElementById("myHover").style.transitionProperty = "width, height";     // Standard syntax
+//this is the function to change the paragraph text color from pink to green when clicked on.
+function mouseDown() {
+    document.getElementById("textChange").style.color = "pink";
 }
 
-$("button")
- .on("mousedown", function(){
-    $("#message").text("mousedown");
-})
-.on("click", function() {
-    $("#message").text("click");
-});
+function mouseUp() {
+    document.getElementById("textChange").style.color = "green";
+}
+
+function myFunction(){
+// Code for Safari 3.1 to 6.0
+document.getElementById("myHover").style.WebkitTransitionProperty = "width, height";
+// Standard syntax
+
+test = document.getElementById("myHover");
+    test.style.transitionProperty = "width, height";
+}
 
 
 /* This is supposed to take an input text field and trigger the alert */
@@ -24,6 +29,14 @@ input.addEventListener("keyup", function(event) {
         // Cancel the default action, if needed
         event.preventDefault();
         // Trigger the button element with a click
+        document.getElementById("myBtn").click();
+    }
+});
+
+var input = document.getElementById("myInput");
+input.addEventListener("keyup", function(event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
         document.getElementById("myBtn").click();
     }
 });
