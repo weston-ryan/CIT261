e8d2351e1a5a31d75bb2ce3476dde714cabe81e6
weston-ryan <31858652+weston-ryan@users.noreply.github.com>
2019-07-08 20:02:43 -0600

    Update application.js

--- application.js
+++ application.js
@@ -1,12 +1,16 @@
 //API link:
 //https://www.commercialloandirect.com/rates-api.html
-function loadXMLDoc(){var e;e=window.XMLHttpRequest?new XMLHttpRequest:new ActiveXObject("Microsoft.XMLHTTP"),e.onreadystatechange=function(){4==e.readyState&&200==e.status&&(document.getElementById("KeyMarketRates").innerHTML=e.responseText)},e.open("POST","https://www.commercialloandirect.com/rates-api.php",!0),e.setRequestHeader("Content-type","application/x-www-form-urlencoded"),e.send("Prime=Y&Libor30Day=N&Libor90Day=N&Libor6Month=N&Libor1Year=N&Swap3Year=N&Swap5Year=N&Swap7Year=N&Swap10Year=N&Treasury5Year=Y&Treasury7Year=N&Treasury10Year=N&SBA10Year=N&SBA20Year=N&Bordered=Y&Center=Y&RightCol=Y&LeftCol=Y&ColorText=0000ff&Width=050&WidthCol1=050&WidthCol2=050&TbColor=90ee90&BdColor=228b22")}
+
+function loadXMLDoc(){
+    var e;e=window.XMLHttpRequest?new XMLHttpRequest:new ActiveXObject("Microsoft.XMLHTTP"),e.onreadystatechange=function(){4==e.readyState&&200==e.status&&(document.getElementById("KeyMarketRates").innerHTML=e.responseText);console.log(e.responseText);},e.open("POST","https://www.commercialloandirect.com/rates-api.php",!0),e.setRequestHeader("Content-type","application/x-www-form-urlencoded"),e.send("Prime=Y&Libor30Day=N&Libor90Day=N&Libor6Month=N&Libor1Year=N&Swap3Year=N&Swap5Year=N&Swap7Year=N&Swap10Year=N&Treasury5Year=Y&Treasury7Year=N&Treasury10Year=N&SBA10Year=N&SBA20Year=N&Bordered=Y&Center=Y&RightCol=Y&LeftCol=Y&ColorText=0000ff&Width=050&WidthCol1=050&WidthCol2=050&TbColor=90ee90&BdColor=228b22")}
 loadXMLDoc();
 
 
 
 
-    function createSavings() {
+
+/*
+function createSavings() {
     if (typeof(Storage) !== "undefined") {
         if (sessionStorage.clickcount) {
             sessionStorage.clickcount = Number(sessionStorage.clickcount)+1;
@@ -18,8 +22,8 @@ loadXMLDoc();
         document.getElementById("result").innerHTML = "Sorry, your browser does not support web storage...";
     }
 }
+*/
 
-/*
 //creating another function trying to do a calculation
 function createSavings() {
     var i1 = document.getElementById('paycheck').value;
@@ -28,13 +32,31 @@ function createSavings() {
     var e2 = document.getElementById('mortgage').value;
     var e3 = document.getElementById('insurance').value;
     var intemCost = document.getElementById('itemCost');
-    //I don't know how to grab just the intrest rate for this
-    var intRate = document.getElementById('Prime').value;
-    var total = (i1 + i2) - (e1 + e2 + e3);
-    var manyMonths = itemCost / total;
 
-if (oper !== "undefined") {
-    document.getElementById("result").innerHTML = "You need to save " + manyMonths + "months to afford the" itemWanted;
+    //var intRate = document.getElementById('Prime').value;
+    var ratesRow = document.getElementsByClassName("ratesRow");
+    var parsedIntRate = ratesRow[0].children[1].innerText;
+    var intRate = parsedIntRate.replace("%","");
+
+    // this part will get the value out of a select list
+    var itemWanted = document.getElementById("itemWanted");
+
+    //var total = (i1 + i2) - (e1 + e2 + e3);
+    var manyMonths = itemCost / ((i1 + i2) - (e1 + e2 + e3));
+    console.log(intRate);
+
+
+
+    var resultText = "You need to save " + manyMonths + " months to afford the " + itemWanted;
+    document.getElementById("result").innerHTML = resultText
+
+
+
+    // this part hides/shows the cantAffordThis.giff
+    var x = document.getElementById("cantAffordThis.gif");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
 }
-}
-*/
